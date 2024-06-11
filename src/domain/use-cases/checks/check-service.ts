29407bd586703230ef2bfcbe84b9244224030fc2
Oscar Ventura <ovenor11@proton.me>
2024-06-11 12:25:38 +0200

    file system repo

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -1,3 +1,6 @@
+import { LogEntity, LogSeverityLevel } from '../../entities/log.entity';
+import { LogRepository } from '../../repository/log.repository';
+
 interface CheckServiceUseCase {
   execute(url: string): Promise<boolean>;
 }
@@ -7,6 +10,7 @@ type ErrorCallback = (error: string) => void;
 
 export class CheckService implements CheckServiceUseCase {
   constructor(
+    private readonly logRepository: LogRepository,
     private readonly successCallback: SuccessCallback,
     private readonly errorCallback: ErrorCallback
   ) {}
@@ -19,11 +23,18 @@ export class CheckService implements CheckServiceUseCase {
         throw new Error(`ERROR ON CHECK SERVICE: ${url}`);
       }
 
+      const log = new LogEntity(`SERVICE ${url} WORKING`, LogSeverityLevel.low);
+      this.logRepository.saveLog(log);
+
       this.successCallback();
 
       return true;
     } catch (error) {
-      this.errorCallback(`${error}`);
+      const errorMessage = `${error}`;
+      const log = new LogEntity(errorMessage, LogSeverityLevel.low);
+      this.logRepository.saveLog(log);
+
+      this.errorCallback(errorMessage);
       return false;
     }
   }
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,11 @@
 import { CheckService } from '../domain/use-cases/checks/check-service';
+import { FileSystemDatasource } from '../infrastructure/datasources/file-system.datasource';
+import { LogRepositoryImpl } from '../infrastructure/repositories/log.repository.impl';
 import { CronService } from './cron/cron-service';
+
+const fileSystemLogRepository = new LogRepositoryImpl(
+  new FileSystemDatasource()
+);
 
 export class Server {
   public static start() {
@@ -9,6 +15,7 @@ export class Server {
       const url = 'http://localhost:3000';
 
       new CheckService(
+        fileSystemLogRepository,
         () => console.log(`${url} IS OK!`),
         (error) => console.log(error)
       ).execute(url);
