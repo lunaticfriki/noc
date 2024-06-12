71b91d9711ea6e73c67f6b112ec0c2fb972a3b4e
Oscar Ventura <ovenor11@proton.me>
2024-06-12 14:48:55 +0200

    mongo log repository

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,28 +1,30 @@
 import { CheckService } from '../domain/use-cases/checks/check-service';
 import { SendEmailLogs } from '../domain/use-cases/email/send-email-logs';
 import { FileSystemDatasource } from '../infrastructure/datasources/file-system.datasource';
+import { MongoLogDatasource } from '../infrastructure/datasources/mongo-log.datasource';
 import { LogRepositoryImpl } from '../infrastructure/repositories/log.repository.impl';
 import { CronService } from './cron/cron-service';
 import { EmailService } from './email/email-service';
 
-const fileSystemLogRepository = new LogRepositoryImpl(
-  new FileSystemDatasource()
+const logRepository = new LogRepositoryImpl(
+  // new FileSystemDatasource()
+  new MongoLogDatasource()
 );
-const emailService = new EmailService();
+// const emailService = new EmailService();
 
 export class Server {
   public static start() {
     console.log('SERVER STARTED!');
 
-    // CronService.createJob('*/5 * * * * *', () => {
-    //   const url = 'http://localhost:3000';
+    CronService.createJob('*/5 * * * * *', () => {
+      const url = 'http://google.com';
 
-    //   new CheckService(
-    //     fileSystemLogRepository,
-    //     () => console.log(`${url} IS OK!`),
-    //     (error) => console.log(error)
-    //   ).execute(url);
-    // });
+      new CheckService(
+        logRepository,
+        () => console.log(`${url} IS OK!`),
+        (error) => console.log(error)
+      ).execute(url);
+    });
 
     // new SendEmailLogs(emailService, fileSystemLogRepository).execute(
     //   'ovenor11@proton.me'
