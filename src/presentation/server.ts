1396ee8a91d94c073d00e4331528b9aea85c9ba7
Oscar Ventura <ovenor11@proton.me>
2024-06-14 13:26:15 +0200

    check multiple services

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,6 @@
 import { LogSeverityLevel } from '../domain/entities/log.entity';
 import { CheckService } from '../domain/use-cases/checks/check-service';
+import { CheckServiceMultiple } from '../domain/use-cases/checks/check-service.multiple';
 import { SendEmailLogs } from '../domain/use-cases/email/send-email-logs';
 import { FileSystemDatasource } from '../infrastructure/datasources/file-system.datasource';
 import { MongoLogDatasource } from '../infrastructure/datasources/mongo-log.datasource';
@@ -8,9 +9,9 @@ import { LogRepositoryImpl } from '../infrastructure/repositories/log.repository
 import { CronService } from './cron/cron-service';
 import { EmailService } from './email/email-service';
 
-const logRepository = new LogRepositoryImpl(
-  // new FileSystemDatasource()
-  // new MongoLogDatasource()
+const fsLogRepository = new LogRepositoryImpl(new FileSystemDatasource());
+const mongoLogRepository = new LogRepositoryImpl(new MongoLogDatasource());
+const postgresLogRepository = new LogRepositoryImpl(
   new PostgresLogDatasource()
 );
 // const emailService = new EmailService();
@@ -22,8 +23,8 @@ export class Server {
     CronService.createJob('*/5 * * * * *', () => {
       const url = 'http://google.com';
 
-      new CheckService(
-        logRepository,
+      new CheckServiceMultiple(
+        [fsLogRepository, mongoLogRepository, postgresLogRepository],
         () => console.log(`${url} IS OK!`),
         (error) => console.log(error)
       ).execute(url);
