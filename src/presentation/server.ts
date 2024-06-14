9cb1de45b35a369f3161a8b71a3dfa6ab23b87f6
Oscar Ventura <ovenor11@proton.me>
2024-06-14 13:28:06 +0200

    remove unused code

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,20 +1,15 @@
-import { LogSeverityLevel } from '../domain/entities/log.entity';
-import { CheckService } from '../domain/use-cases/checks/check-service';
 import { CheckServiceMultiple } from '../domain/use-cases/checks/check-service.multiple';
-import { SendEmailLogs } from '../domain/use-cases/email/send-email-logs';
 import { FileSystemDatasource } from '../infrastructure/datasources/file-system.datasource';
 import { MongoLogDatasource } from '../infrastructure/datasources/mongo-log.datasource';
 import { PostgresLogDatasource } from '../infrastructure/datasources/postgres-log.datasource';
 import { LogRepositoryImpl } from '../infrastructure/repositories/log.repository.impl';
 import { CronService } from './cron/cron-service';
-import { EmailService } from './email/email-service';
 
 const fsLogRepository = new LogRepositoryImpl(new FileSystemDatasource());
 const mongoLogRepository = new LogRepositoryImpl(new MongoLogDatasource());
 const postgresLogRepository = new LogRepositoryImpl(
   new PostgresLogDatasource()
 );
-// const emailService = new EmailService();
 
 export class Server {
   public static async start() {
